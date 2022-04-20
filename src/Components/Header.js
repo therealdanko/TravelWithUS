3262bb85a2d53bf9f8d04129d60cd5d21bdaab5f
Danko Karleusa <dankomatichevich@gmail.com>
2022-04-20 14:19:31 -0700

    css structure

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <div>
+        <h1 className="header">Travel With US</h1>
+    </div>
+  )
+}
+
+export default Header
--- src/Components/LeafMap.js
+++ src/Components/LeafMap.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks'
 import { Marker } from 'react-leaflet/Marker'
 import {Popup} from 'react-leaflet/Popup'
 import './LeafMap.css'
