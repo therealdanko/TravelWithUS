9c507ba841068b77f9bb2487dfcc911dd6a5f7cd
kale <kalerampenthal@gmail.com>
2022-04-21 08:57:34 -0600

    added delete button and adjusted css

--- src/Components/LeafMap.js
+++ src/Components/LeafMap.js
@@ -19,23 +19,33 @@ const LeafMap = () => {
     .then(res => setData(res))
   }, [])
 
-
+  const onDelete = (id) => {
+    console.log(id)
+    fetch(URL + id, {method: 'DELETE'})
+    .then((response) => response.json())
+    const newArray = data.filter(place => {
+      if (place.id !== id) {
+        return place
+      }
+    })
+    setData(newArray)
+  }
 
   return (
     <div>
-      <MapContainer center={[38.8283, -95.5795]} zoom={4.5} scrollWheelZoom={false}>
+      <MapContainer center={[38.8283, -95.5795]} zoom={4.5} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {data.map(point => {
           return <Marker key={point.id} position={[point.geolocation[0], point.geolocation[1]]}>
-              <Popup>
-                <div className="pop-up">
+              <Popup className='pop-up-container'>
+                <div className="pop-up-info">
                   <h4>{point.title}</h4>
                   <img style={{height: 150}} src={point.image} alt="" />
                   <p>{point.description}</p>
-                  <Button variant="contained">Delete</Button>
+                  <Button onClick={() => onDelete(point.id)} variant="contained">Delete</Button>
                 </div>
               </Popup>
             </Marker>
@@ -46,3 +56,4 @@ const LeafMap = () => {
 }
 
 export default LeafMap
+
